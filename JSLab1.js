64b864cbf861d932803f99434ce0715bb82703f2
NGF2020 <v.strutynskyj@gmail.com>
2025-03-13 10:07:46 +0200

    first commit

---
+++ JSLab1.js
@@ -0,0 +1,21 @@
+function isInteger(num) {
+    return num % 1 === 0;
+}
+
+let uInput = prompt("");
+let number = Number(uInput);
+
+if (isInteger(number)) {
+    alert("t");
+} else {
+    alert("f");
+}
+
+function findNums(a, b) {
+    let Nums = [];
+
+    function isNums(n) {
+        let isNums = true;
+        if (n < 2) return false;
+    }
+}
